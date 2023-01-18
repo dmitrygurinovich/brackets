fe13ef0c68c5dec2a2ba578c7a1a43a524c601db
Dmitry Gurinovich <dmitry.gurinovich@hotmail.com>
2023-01-18 11:28:31 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let buffer = [];
+
+	function isOpeningBracket(bracket) {
+		for (let i = 0; i < bracketsConfig.length; i++) {
+			if (bracketsConfig[i][0] != bracketsConfig[i][1] && bracket == bracketsConfig[i][0]) {
+				return true;
+			} else if (bracketsConfig[i][0] != bracketsConfig[i][1] && bracket == bracketsConfig[i][1]) {
+				return false;
+			} else if (bracketsConfig[i][0] === bracketsConfig[i][1] && bracket === bracketsConfig[i][0]) {
+
+				if (buffer.length == 0) {
+					return true;
+				} else {
+					if (bracket === buffer[buffer.length - 1]) {
+						return false;
+					} else {
+						return true;
+					}
+				}
+			}
+		}
+	}
+
+	function searchOpeningBracketByClosing(bracket) {
+		for (let i = 0; i < bracketsConfig.length; i++) {
+			if (bracketsConfig[i][1] === bracket) {
+				return bracketsConfig[i][0];
+			}
+		}
+	}
+
+	for (let a = 0; a < str.length; a++) {
+		if (str.length % 2 !== 0) {
+			return false;
+		}
+		if (isOpeningBracket(str.charAt(a))) {
+			buffer.push(str.charAt(a));
+		} else {
+			if (buffer.length == 0) {
+				return false;
+			} else {
+				if (searchOpeningBracketByClosing(str[a]) !== buffer[buffer.length - 1]) {
+					return false;
+				} else {
+					buffer.pop();
+				}
+			}
+		}
+	}
+
+	if (buffer.length == 0) {
+		return true;
+	} else {
+		return false;
+	}
 }
